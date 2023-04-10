652b66dbc2437ebc050c3b9f9bbbd1eb9ad14cc1
LinuxJeff <dev.jeferson.bahia@gmail.com>
2023-04-10 18:55:41 -0300

    v3

--- POKEDEX/assets/js/main.js
+++ POKEDEX/assets/js/main.js
@@ -1,1 +1,10 @@
-console.log("Hi lorena")
+
+
+
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url).then(function(){
+    console.log(Response)
+})
