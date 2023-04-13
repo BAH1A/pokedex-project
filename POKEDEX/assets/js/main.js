02c5122e6b509bf2e5195a4ccd67d46895721900
BAH1A <dev.jeferson.bahia@gmail.com>
2023-04-13 12:41:20 -0300

    Update

--- POKEDEX/assets/js/main.js
+++ POKEDEX/assets/js/main.js
@@ -5,6 +5,16 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url).then(function(){
-    console.log(Response)
-})
+fetch(url)
+    .then(function(response){
+        return response.json()
+    }) /*.then((response) => response.json()) */
+    .then(function(jsonBody){
+        console.log(jsonBody)
+    }) /*.then((jsonBody) => console.log(jsonBody)) */
+    .catch(function(error){
+        console.log(error)
+    })/*.catch((error) => console.error(error)) */
+    .finally(function(){
+        console.log(`Requisição feita com sucesso`)
+    })
